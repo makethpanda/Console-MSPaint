dc6a768c324ac0e444a471d22be0ecc39efd5b9f
Julien <42862794+makethpanda@users.noreply.github.com>
2023-04-12 15:23:07 +0200

    first displaymenu function

---
+++ functions.c
@@ -0,0 +1,13 @@
+//
+// Created by jboud on 12/04/2023.
+//
+#include <stdio.h>
+#include "functions.h"
+
+// Function to display the menu options
+void displayMenu() {
+    printf("Please choose an option from the menu:\n");
+    printf("1. App\n");
+    printf("2. Instructions\n");
+    printf("3. Quit\n");
+}
